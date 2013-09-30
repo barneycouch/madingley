f7290c91ddf5611973e6447dedc00425fb2fa51a
Barney Couch <barneycouch@gmail.com>
2013-09-30 13:56:58 +0100

    wrapped in a function

--- chimps/rcode/bloodVpoop.R
+++ chimps/rcode/bloodVpoop.R
@@ -1,13 +1,17 @@
 library(ggplot2)
+library(reshape)
 setwd("~/Dropbox/code/madingley/chimps/chimp-DatasetS1S2/Dataset_S2")
 chr21 <- read.table("Chr21Genotypes.txt",head=T)
 
-peanutblood <- as.data.frame(table(chr21$Blood.peanut))
-peanutpoop <- as.data.frame(table(chr21$Poop.peanut))
-peanutboth <- merge(peanutblood, peanutpoop, by="Var1")
-names(peanutboth)[2] <- "Blood"
-names(peanutboth)[3] <- "Poop"
-#ggplot(peanutboth,aes(x=Var1)) + geom_point(size=3, colour="#CC0000",aes(y=Freq.x)) + geom_point(size=3, aes(y=Freq.y)) + xlab("Base Substitution") + ylab("Frequency")
-peanutboth.long <- melt(peanutboth, id.vars="Var1")
-names(peanutboth.long)[2] <- "sample.origin"
-ggplot(peanutboth.long, aes(Var1, value, colour=sample.origin))+ geom_point(size=3) + xlab("Base Substitution") + ylab("Frequency")
+compareBloodPoop <- function(chimpname) {
+  bloodcol <- paste("Blood.", chimpname, sep="")
+  poopcol <- paste("Poop.", chimpname, sep="")
+  bloodtable <- as.data.frame(table(chr21[,bloodcol]))
+  pooptable <- as.data.frame(table(chr21[,poopcol]))
+  bothtable <- merge(bloodtable, pooptable, by="Var1")
+  names(bothtable)[2] <- "Blood"
+  names(bothtable)[3] <- "Poop"
+  bothtable.long <- melt(bothtable, id.vars="Var1")
+  names(bothtable.long)[2] <- "sample.origin"
+  return(ggplot(bothtable.long, aes(Var1, value, colour=sample.origin)) + geom_point(size=3) + xlab("Base Substitution") + ylab("Frequency"))
+}
