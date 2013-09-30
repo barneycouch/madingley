b93af8d6ba68c5defbe48724deb7fb898a73a42a
Barney Couch <barneycouch@gmail.com>
2013-09-30 14:39:01 +0100

    graphs and finished functional script

--- chimps/rcode/bloodVpoop.R
+++ chimps/rcode/bloodVpoop.R
@@ -13,5 +13,8 @@ compareBloodPoop <- function(chimpname) {
   names(bothtable)[3] <- "Poop"
   bothtable.long <- melt(bothtable, id.vars="Var1")
   names(bothtable.long)[2] <- "sample.origin"
-  return(ggplot(bothtable.long, aes(Var1, value, colour=sample.origin)) + geom_point(size=3) + xlab("Base Substitution") + ylab("Frequency"))
+  chimpname <- paste(toupper(substring(chimpname, 1,1)), substring(chimpname, 2), sep="") #capitalise first letter of chimp name
+  #return(ggplot(bothtable.long, aes(Var1, value, colour=sample.origin)) + geom_point(size=3) + xlab("Base Substitution") + ylab("Frequency") + ggtitle(paste("Frequency Differences in", chimpname)))
+  ggplot(bothtable.long, aes(Var1, value, colour=sample.origin)) + geom_point(size=3) + xlab("Base Substitution") + ylab("Frequency") + ggtitle(paste("Frequency Differences in", chimpname))
+  ggsave(file=paste(chimpname, ".jpg", sep=""), path="/Users/Barney/Dropbox/code/madingley/chimps/rcode")
 }
