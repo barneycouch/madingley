de9c0912c57ad582af86642296bbba7c94a8b712
Barney Couch <barneycouch@gmail.com>
2013-09-30 13:42:52 +0100

    improve plotting

--- chimps/rcode/bloodVpoop.R
+++ chimps/rcode/bloodVpoop.R
@@ -18,4 +18,9 @@ peanutblood <- as.data.frame(table(chr21$Blood.peanut))
 peanutpoop <- as.data.frame(table(chr21$Poop.peanut))
 #ggplot(peanutblood,aes(x=Var1, y=Freq)) + geom_point()
 peanutboth <- merge(peanutblood, peanutpoop, by="Var1")
-ggplot(peanutboth,aes(x=Var1)) + geom_point(size=3, colour="#CC0000",aes(y=Freq.x)) + geom_point(size=3, aes(y=Freq.y)) + xlab("Base Substitution") + ylab("Frequency")
+names(peanutboth)[2] <- "Blood"
+names(peanutboth)[3] <- "Poop"
+#ggplot(peanutboth,aes(x=Var1)) + geom_point(size=3, colour="#CC0000",aes(y=Freq.x)) + geom_point(size=3, aes(y=Freq.y)) + xlab("Base Substitution") + ylab("Frequency")
+peanutboth.long <- melt(peanutboth, id.vars="Var1")
+names(peanutboth.long)[2] <- "sample.origin"
+ggplot(peanutboth.long, aes(Var1, value, colour=sample.origin))+ geom_point(size=3) + xlab("Base Substitution") + ylab("Frequency")
